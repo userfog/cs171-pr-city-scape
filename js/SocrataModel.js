2bf9456b2261c8cf526e9c73a048f7efa7ec2e3d
Zach Fogelson <fogelsonz606@gmail.com>
2015-04-12 12:13:24 -0400

    General idea of Choropleth works need to polish it up

--- js/SocrataModel.js
+++ js/SocrataModel.js
@@ -1,19 +1,24 @@
 
 
-var SocrataModel = function(_baseUrl, _resource, _apiKey, _responseType){
+var SocrataModel = function(_baseUrl, _resource, _apiKey, _eventHandler, _responseType){
   this.baseUrl = _baseUrl;
   this.resource = _resource;
   this.apiKey = _apiKey;
   this.responseType = _responseType || "json";
   this.fullUrl = "{0}/resource/{1}.{2}?".format(this.baseUrl, this.resource, this.responseType);
+  this.eventHandler = _eventHandler;
+  this.data;
 }
 
 SocrataModel.prototype.get = function (str, callback){
+    var that = this;
     $.getJSON(this.fullUrl
         + str 
         + "&$$app_token=" + this.apiKey,
       callback || function(data, status) {
-                  console.log("Request received: " + data);
+                  that.map = d3.map();
+                  data.forEach(function(d){that.map.set(+d.community_area, +d.count_primary_type)})
+                  $(that.eventHandler).trigger("selectionChanged", []);
                 }
     ).fail(function() {
       console.log("Something went wrong!");
