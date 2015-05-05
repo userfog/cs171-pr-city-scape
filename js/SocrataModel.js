663770c1b2cf5013d10a3b7f6836bb23fbf904c2
Zach Fogelson <fogelsonz606@gmail.com>
2015-05-04 20:30:02 -0400

    Lining up a bit more

--- js/SocrataModel.js
+++ js/SocrataModel.js
@@ -26,7 +26,7 @@ SocrataModel.prototype.get = function (str, callback, offset, limit, clear){
       function(data, status) {
         that.data.push.apply(that.data, data);
         state.length = state.length + data.length  || data.length
-        if(data.length < limit){
+        if(data.length < limit || true){
           safe_callback(callback, that);
         } else{
           that.get(str, callback, offset+limit, limit, false);
